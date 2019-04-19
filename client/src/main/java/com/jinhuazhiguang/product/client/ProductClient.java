f1d5d74ce5ebc37be8ee788cd70e07edeb374fd0
pdpdgh <18823886787@qq.com>
2019-04-20 03:16:24 +0800

    SpringCloud 10-7 finished

--- client/src/main/java/com/jinhuazhiguang/product/client/ProductClient.java
+++ client/src/main/java/com/jinhuazhiguang/product/client/ProductClient.java
@@ -3,6 +3,7 @@ package com.jinhuazhiguang.product.client;
 import com.jinhuazhiguang.product.common.DecreaseStockInput;
 import com.jinhuazhiguang.product.common.ProductInfoOutput;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -13,7 +14,7 @@ import java.util.List;
  * @auth : pdp
  * @date : 2019/04/05 13:30
  */
-@FeignClient(name = "product")
+@FeignClient(name = "product", fallback = ProductClient.ProductClientFallback.class)
 public interface ProductClient {
 
     @PostMapping("/product/listForOrder")
@@ -21,4 +22,18 @@ public interface ProductClient {
 
     @PostMapping("/product/decreaseStock")
     void decreaseStock(@RequestBody List<DecreaseStockInput> decreaseStockInputList);
+
+    @Component
+    static class ProductClientFallback implements ProductClient {
+
+        @Override
+        public List<ProductInfoOutput> listForOrder(List<String> productIdList) {
+            return null;
+        }
+
+        @Override
+        public void decreaseStock(List<DecreaseStockInput> decreaseStockInputList) {
+
+        }
+    }
 }
